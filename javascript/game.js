6239f78d17d293ff375c345b2a5718884cd17332
EKaye <holmanelisek@gmail.com>
2019-08-27 21:36:20 -0400

    basic button click function

--- javascript/game.js
+++ javascript/game.js
@@ -1,3 +1,42 @@
+$(document).ready(function() {
+    //establish variables
+    var crystalvalues = [0,0,0,0];
+    var goalNumber = 0;
+    var playerScore = 0;
+    var wins = 0;
+    var losses = 0;
+
+    //randomly set goal number between 19 and 120
+    goalNumber = Math.floor(Math.random() * 101) + 19;  
+
+    //runs when a crystal is clicked, with a variable indexing which crystal is selected
+    function crystalclick(x){
+        if(crystalvalues[x]==0){
+            crystalvalues[x]==Math.floor(Math.random() * 12) + 1;
+        }
+        //add crystal value to playerScore
+        playerScore = playerScore + crystalvalue[x];
+        //update player score display
+        //YOU DO NOT HAVE CODE HERE BABE YOU NEED CODE HERE
+        //if player reaches or goes over the goal, they're starting a new game
+        if(playerScore>=goalNumber){
+            //UPDATE DISPLAYS
+            playerScore = 0;
+            crystalvalues = [0,0,0,0];
+            goalNumber = Math.floor(Math.random() * 101) + 19;
+            //if player reaches goal number, win!
+            if(playerScore==goalNumber){
+                wins++;
+            }
+            //if they went over, lose
+            else {
+                losses++;
+            }
+        }
+        //otherwise, just go ahead and click again!
+    }
+});
+
 //game start
 //variables: crystal1value crystal2value crystal3value crystal4value crystalset(bool) goalNumber playerScore wins losses
 //pick random value for randomGoalNumber
