c92d10d1612039cdc53c07fc697ca1978740a419
EKaye <holmanelisek@gmail.com>
2019-08-27 20:51:12 -0400

    pseudocodes and readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # unit-4-game
+_This game assigns random values to a set of 4 crystals, and gives the user a randomly generated goal number._
+_Without going over the goal number, the user must click the crystals to both discover their values and hopefully reach the goal number._
+_If the player reaches the exact goal number, they win!_
+_Otherwise, if the player goes over the goal number, they lose._
---
+++ javascript/game.js
@@ -0,0 +1,23 @@
+//game start
+//variables: crystal1value crystal2value crystal3value crystal4value crystalset(bool) goalNumber playerScore wins losses
+//pick random value for randomGoalNumber
+//when user clicks on any of the crystal buttons (class crystal)
+//if crystalset false, randomly generate numbers 1-12 for each crystal
+//add specific crystal value to playerScore
+//update playerScore display
+//if playerScore == goalNumber
+//  wins++
+//  update wins display
+//  playerScore = 0
+//  update playerScore display
+//  crystalset=false
+//  new goal number
+//  update goalnumber display
+//else if playerScore > goalNumber
+//  losses ++
+//  update losses display
+//  playerScore = 0
+//  update playerScore display
+//  crystalset=false
+//  new goal number
+//  update goal number display
